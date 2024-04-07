3c626b1a543907d5ddc53842c96b02f654f059cc
prathamesh amundkar <prathameshamundkar@gmail.com>
2024-04-07 11:38:16 +0530

    01_prime_number

--- 01_prime_numbers.cpp
+++ 01_prime_numbers.cpp
@@ -1,8 +1,34 @@
+
+// find all prime numbers upto the n 
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
 
+  int n;
+  cout<<"Enter the number: "<<endl;
+  cin>>n;
+
+  vector<int>ans;
+  
+
+  for(int i=2;i<=n;i++){
+     bool check=true;
+      for(int j=2;j<i;j++){
+        
+        if(i%j==0){
+            check=false;
+        }
+      }
+      if(check==true){
+        ans.push_back(i);
+      }
+  }
+
+  for(int i=0;i<ans.size();i++){
+    cout<<ans[i]<<" ";
+  }
 
 
 
